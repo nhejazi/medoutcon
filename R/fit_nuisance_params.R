ce1d574acae60c8dd2e924a217c64467bad2d372
Nima Hejazi <nh@nimahejazi.org>
2019-04-26 21:36:31 -0700

    minor changes

--- R/fit_nuisance_params.R
+++ R/fit_nuisance_params.R
@@ -9,6 +9,10 @@
 #' @param valid_data A holdout data set, with columns exactly matching those
 #'  appearing in the preceding argument \code{data}, to be used for estimation
 #'  via cross-fitting. Optional, defaulting to \code{NULL}.
+#' @param contrast A \code{numeric} double indicating the two values of the
+#'  intervention \code{A} to be compared. The default value of \code{c(0, 1)}
+#'  assumes a binary intervention node \code{A}, though support for categorical
+#'  interventions is planned for future releases.
 #' @param lrnr_stack A \code{Stack} object, or other learner class (inheriting
 #'  from \code{Lrnr_base}), containing a single or set of instantiated learners
 #'  from the \code{sl3} package, to be used in fitting a model for the
@@ -23,31 +27,45 @@
 #'
 fit_u_param <- function(data,
                         valid_data,
+                        contrast,
                         lrnr_stack,
                         m_output,
                         q_output,
                         r_output,
                         e_output) {
-    # extract required elements from fit likelihood components/mechanisms
-    e_natural_pred <- e_output$e_est$e_pred_A_natural
-    e_star_pred <- e_output$e_est$e_pred_A_star
-    q_natural_pred <- q_output$est$pred_natural
-    r_natural_pred <- r_output$est$pred_natural
-    m_natural_pred <- m_output$m_est$m_pred_A_natural
+  # extract required elements from fit likelihood components/mechanisms
+  e_natural_pred <- e_output$e_est$e_pred_A_natural
+  e_star_pred <- e_output$e_est$e_pred_A_star
+  q_natural_pred <- q_output$est$pred_natural
+  r_natural_pred <- r_output$est$pred_natural
+  m_natural_pred <- m_output$m_est$m_pred_A_natural
 
-    # construct outcome component of nuisance parameter
-    u_out <- m_natural_pred * (q_natural_pred / r_natural_pred) *
-        (e_star_pred / e_natural_pred)
+  # construct outcome component of nuisance parameter
+  u_out <- m_natural_pred * (q_natural_pred / r_natural_pred) *
+      (e_star_pred / e_natural_pred)
 
-    # construct task for nuisance parameter regression
-    u_data <- data[, U := u_out]
-    u_task <- sl3::sl3_Task(
-      data = u_data,
-      covariates = c("A", "Z", w_names),
-      outcome = "U"
-    )
+  # use full data for counterfactual prediction if no validation data given
+  if (is.null(valid_data)) {
+    # copy full data
+    data_intervene <- data.table::copy(data)
+  } else {
+    # copy only validation data
+    data_intervene <- data.table::copy(valid_data)
+  }
 
-    # run regression...
+
+  # construct task for nuisance parameter regression
+  u_data <- data[, U := u_out]
+  u_task <- sl3::sl3_Task(
+    data = u_data,
+    covariates = c("A", "Z", w_names),
+    outcome = "U"
+  )
+
+  # fit regression model and evaluate for contrast of interest
+  u_natural_fit <- lrnr_stack$train(u_task)
+  u_intervened_pred <- u_natural_fit$predict(u_intervened_task)
+
 
 }
 
@@ -64,6 +82,10 @@ fit_u_param <- function(data,
 #' @param valid_data A holdout data set, with columns exactly matching those
 #'  appearing in the preceding argument \code{data}, to be used for estimation
 #'  via cross-fitting. Optional, defaulting to \code{NULL}.
+#' @param contrast A \code{numeric} double indicating the two values of the
+#'  intervention \code{A} to be compared. The default value of \code{c(0, 1)}
+#'  assumes a binary intervention node \code{A}, though support for categorical
+#'  interventions is planned for future releases.
 #' @param lrnr_stack A \code{Stack} object, or other learner class (inheriting
 #'  from \code{Lrnr_base}), containing a single or set of instantiated learners
 #'  from the \code{sl3} package, to be used in fitting a model for the
@@ -76,6 +98,7 @@ fit_u_param <- function(data,
 #'
 fit_v_param <- function(data,
                         valid_data,
+                        contrast,
                         lrnr_stack,
                         m_output,
                         q_output) {

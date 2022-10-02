e20774d40b1cfc2bd0686e803e833c782289cd6b
Philippe Boileau <pboileau.swim@gmail.com>
2022-10-01 21:07:33 -0700

    two-phase sampling natural params estimate checks

--- tests/testthat/test-two-phase-sampling.R
+++ tests/testthat/test-two-phase-sampling.R
@@ -14,7 +14,7 @@ test_that("two_phase_eif returns an uncentered EIF", {
 
   # independently compute the adjusted EIF
   uncentered_eif <- S * two_phase_weights * eif +
-    (1 - S * two_phase_weights) * eif_predictions - plugin_est
+    (1 - S * two_phase_weights) * eif_predictions + plugin_est
 
   # assert that result is identical to two_phase_eif's
   expect_equal(
@@ -99,7 +99,7 @@ test_that("MSE of propensity score estimates is sufficiently low", {
 
 ## fit propensity score conditioning on mediators
 h_out <- fit_treat_mech(
-  train_data = full_data, valid_data = full_data, contrast = c(aprime, astar),
+  train_data = data, valid_data = data, contrast = c(aprime, astar),
   learners = h_learners, w_names = w_names,
   m_names = m_names, type = "h"
 )
@@ -115,7 +115,7 @@ test_that("MSE of mediator propensity score estimates is sufficiently low", {
 
 ## fit outcome regression
 b_out <- fit_out_mech(
-  train_data = full_data, valid_data = full_data, contrast = c(aprime, astar),
+  train_data = data, valid_data = data, contrast = c(aprime, astar),
   learners = b_learners, m_names = m_names, w_names = w_names
 )
 test_that("Estimates of outcome regression are close to the truth", {
@@ -130,8 +130,8 @@ test_that("MSE of outcome regression estimates is sufficiently low", {
 
 ## fit mediator-outcome confounder regression, excluding mediator(s)
 q_out <- fit_moc_mech(
-  train_data = full_data,
-  valid_data = full_data,
+  train_data = data,
+  valid_data = data,
   contrast = contrast,
   learners = q_learners,
   m_names = m_names,
@@ -151,8 +151,8 @@ test_that("MSE of confounder regression q estimates is sufficiently low", {
 
 ## fit mediator-outcome confounder regression, conditioning on mediator(s)
 r_out <- fit_moc_mech(
-  train_data = full_data,
-  valid_data = full_data,
+  train_data = data,
+  valid_data = data,
   contrast = contrast,
   learners = r_learners,
   m_names = m_names,
@@ -177,7 +177,7 @@ data_a_star <- data.table::copy(data)[, A := astar]
 
 # fit u
 u_out <- fit_nuisance_u(
-  train_data = full_data,
+  train_data = data,
   valid_data = data_a_prime,
   learners = u_learners,
   b_out = b_out,
@@ -197,7 +197,7 @@ test_that("MSE of pseudo-outcome regression estimates is sufficiently low", {
 
 ## fit v
 v_out <- fit_nuisance_v(
-  train_data = full_data,
+  train_data = data,
   valid_data = data_a_star,
   contrast = contrast,
   learners = v_learners,
@@ -226,8 +226,8 @@ test_that("MSE of pseudo-outcome used in v estimation is sufficiently low", {
 
 ## fit eif regression
 d_out <- fit_nuisance_d(
-  train_data = full_data,
-  valid_data = full_data,
+  train_data = data,
+  valid_data = data,
   contrast = contrast,
   learners = d_learners,
   b_out = b_out,
@@ -242,13 +242,13 @@ d_out <- fit_nuisance_d(
 )
 
 ## compute full data eif values
-full_data[, S := 1]
-folds <- make_folds(full_data, fold_fun = folds_vfold, V = 1)
+data[, S := 1]
+folds <- make_folds(data, fold_fun = folds_vfold, V = 1)
 fold_obj <- folds[[1]]
 fold_obj$training_set <- fold_obj$validation_set
 eif <- cv_eif(
   fold = fold_obj,
-  data = full_data,
+  data = data,
   contrast = contrast,
   g_learners = g_learners,
   h_learners = h_learners,
@@ -270,4 +270,192 @@ test_that("Estimates of the full-data EIF are close to the full-data EIF", {
 test_that("MSE of pseudo-outcome used in v estimation is sufficiently low", {
   v_pseudo_mse <- mean((v_out$v_pseudo - intv(m, w, aprime))^2)
   expect_lt(mean((d_out$d_pred - full_data_eif)^2), 0.1)
+})
+
+################################################################################
+# Unit tests for estimators
+################################################################################
+
+context(paste("Estimators of natural effects are close to true parameters in",
+              "two-phase sampling designs"))
+source("utils_natural.R")
+
+# packages
+library(data.table)
+library(stringr)
+library(tibble)
+library(dplyr)
+library(hal9001)
+library(glmnet)
+library(sl3)
+library(SuperLearner)
+
+# options
+set.seed(618394)
+n_obs <- 5000
+
+# 1) get data and column names for sl3 tasks (for convenience)
+data <- make_nide_data(n_obs = n_obs)
+data[, `:=`(S = rbinom(n_obs, 1, 0.9),
+            two_phase_weights = 1,
+            obs_weights = 1)]
+w_names <- str_subset(colnames(data), "W")
+m_names <- str_subset(colnames(data), "Z")
+
+# 2) use simpler SLs for testing functionality
+mean_lrnr <- Lrnr_mean$new()
+fglm_lrnr <- Lrnr_glm_fast$new()
+lasso_lrnr <- Lrnr_glmnet$new(alpha = 1, nfolds = 3L)
+enet_lrnr <- Lrnr_glmnet$new(alpha = 0.5, nfolds = 3L)
+rf_lrnr <- Lrnr_ranger$new(
+  num.trees = 1000, sample.fraction = 0.7,
+  oob.error = FALSE
+)
+logistic_meta <- Lrnr_solnp$new(
+  metalearner_logistic_binomial,
+  loss_loglik_binomial
+)
+sl_binary <- Lrnr_sl$new(
+  learners = list(
+    rf_lrnr,
+    fglm_lrnr,
+    lasso_lrnr,
+    enet_lrnr,
+    mean_lrnr
+  ),
+  metalearner = logistic_meta
+)
+sl_contin <- Lrnr_sl$new(
+  learners = list(
+    rf_lrnr,
+    fglm_lrnr,
+    lasso_lrnr,
+    enet_lrnr,
+    mean_lrnr
+  ),
+  metalearner = Lrnr_nnls$new()
+)
+
+## nuisance functions with data components have binary outcomes
+g_learners <- h_learners <- q_learners <- r_learners <- rf_lrnr
+
+## nuisance functions with pseudo-outcomes have continuous outcomes
+d_learners <- u_learners <- v_learners <- b_learners <- rf_lrnr
+
+
+# 3) test different estimators
+nde_os <- medoutcon(
+  W = data[, ..w_names], A = data$A, Z = NULL,
+  M = data[, ..m_names], Y = data$Y, S = data$S,
+  g_learners = g_learners,
+  h_learners = h_learners,
+  b_learners = b_learners,
+  q_learners = q_learners,
+  r_learners = r_learners,
+  u_learners = u_learners,
+  v_learners = v_learners,
+  d_learners = d_learners,
+  effect = "direct",
+  estimator = "onestep",
+  estimator_args = list(cv_folds = 5, max_iter = 0, tiltmod_tol = 5)
+)
+summary(nde_os)
+
+nie_os <- medoutcon(
+  W = data[, ..w_names], A = data$A, Z = NULL,
+  M = data[, ..m_names], Y = data$Y, S = data$S,
+  g_learners = g_learners,
+  h_learners = h_learners,
+  b_learners = b_learners,
+  q_learners = q_learners,
+  r_learners = r_learners,
+  u_learners = u_learners,
+  v_learners = v_learners,
+  d_learners = d_learners,
+  effect = "indirect",
+  estimator = "onestep",
+  estimator_args = list(cv_folds = 5, max_iter = 0, tiltmod_tol = 5)
+)
+summary(nie_os)
+
+nde_tmle <- medoutcon(
+  W = data[, ..w_names], A = data$A, Z = NULL,
+  M = data[, ..m_names], Y = data$Y, S = data$S,
+  g_learners = g_learners,
+  h_learners = h_learners,
+  b_learners = b_learners,
+  q_learners = q_learners,
+  r_learners = r_learners,
+  u_learners = u_learners,
+  v_learners = v_learners,
+  d_learners = d_learners,
+  effect = "direct",
+  estimator = "tmle",
+  estimator_args = list(cv_folds = 5, max_iter = 10, tiltmod_tol = 10)
+)
+summary(nde_tmle)
+
+nie_tmle <- medoutcon(
+  W = data[, ..w_names], A = data$A, Z = NULL,
+  M = data[, ..m_names], Y = data$Y, S = data$S,
+  g_learners = g_learners,
+  h_learners = h_learners,
+  b_learners = b_learners,
+  q_learners = q_learners,
+  r_learners = r_learners,
+  u_learners = u_learners,
+  v_learners = v_learners,
+  d_learners = d_learners,
+  effect = "indirect",
+  estimator = "tmle",
+  estimator_args = list(cv_folds = 5, max_iter = 10, tiltmod_tol = 10)
+)
+summary(nie_tmle)
+
+
+# 4) compute approximate truth and efficiency bound
+sim_truth <- get_truth_nide(
+  n_obs = 1e6, binary_outcome = FALSE, EIC = TRUE
+)
+EY_A1_Z1 <- sim_truth$EY_A1_Z1
+EY_A1_Z0 <- sim_truth$EY_A1_Z0
+EY_A0_Z1 <- sim_truth$EY_A0_Z1
+EY_A0_Z0 <- sim_truth$EY_A0_Z0
+nde_true <- mean(EY_A1_Z0 - EY_A0_Z0)
+nie_true <- mean(EY_A1_Z1 - EY_A1_Z0)
+
+
+# 5) testing estimators for the NDE
+test_that("NDE: One-step estimate is near DGP truth", {
+  expect_equal(nde_os$theta, nde_true, tol = 0.05)
+})
+
+test_that("NDE: TML estimate is near DGP truth", {
+  expect_equal(nde_tmle$theta, nde_true, tol = 0.05)
+})
+
+test_that("NDE: Mean of estimated EIF is nearly zero for the one-step", {
+  expect_lt(abs(mean(nde_os$eif)), 1e-15)
+})
+
+test_that("NDE: Mean of estimated EIF is approximately solved for the TMLE", {
+  expect_lt(abs(mean(nde_tmle$eif)), var(nde_tmle$eif) / (n_obs * log(n_obs)))
+})
+
+
+# 6) testing estimators for the NIE
+test_that("NIE: One-step estimate is near DGP truth", {
+  expect_equal(nie_os$theta, nie_true, tol = 0.05)
+})
+
+test_that("NIE: TML estimate is near DGP truth", {
+  expect_equal(nie_tmle$theta, nie_true, tol = 0.05)
+})
+
+test_that("NIE: Mean of estimated EIF is nearly zero for the one-step", {
+  expect_lt(abs(mean(nie_os$eif)), 1e-15)
+})
+
+test_that("NIE: Mean of estimated EIF is approximately solved for the TMLE", {
+  expect_lt(abs(mean(nie_tmle$eif)), var(nie_tmle$eif) / (n_obs * log(n_obs)))
 })
